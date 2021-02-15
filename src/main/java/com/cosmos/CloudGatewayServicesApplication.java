53c45e984ae33e1c9df65a54b12e3296d4902bda
Abhijit <abhijitmishra2632@gmail.com>
2021-02-15 10:11:23 +0530

    updated for hystrix

--- src/main/java/com/cosmos/CloudGatewayServicesApplication.java
+++ src/main/java/com/cosmos/CloudGatewayServicesApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CloudGatewayServicesApplication {
 
 	public static void main(String[] args) {
